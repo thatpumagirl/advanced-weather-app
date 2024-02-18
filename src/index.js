7929fc3bac5c8b4560f5c1b04f4586bcf7a4b920
thatpumagirl <joymesh35@gmail.com>
2024-02-18 20:25:48 +0300

    intergrated the API with the Js file

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -52,7 +54,15 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+  let apiKey = '351o8t0b2f4f1918b40aaf94b9e80758';
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
   let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let forecastHtml = '';
 
@@ -81,4 +91,3 @@ let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Nairobi');
-displayForecast();
