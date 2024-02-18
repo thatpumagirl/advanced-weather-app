bd646b0e257207194496706efe9802cf63ed5212
thatpumagirl <joymesh35@gmail.com>
2024-02-18 23:16:32 +0300

    Changed both Js and Css files
    Css was tweaked to increase icon size and changed colors
    Looped through daily forecast in Js file

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = '351o8t0b2f4f1918b40aaf94b9e80758';
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -63,24 +70,27 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let forecastHtml = '';
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌥️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src='${day.condition.icon_url}' class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
         </div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector('#forecast');
@@ -91,3 +101,4 @@ let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Nairobi');
+displayForecast();
